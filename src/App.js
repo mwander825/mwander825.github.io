9761a1276a48a17e7d75ebefd7fd415330bd6a93
mwander825 <mjwander211@gmail.com>
2022-09-04 17:29:08 -0400

    Creating "about" page

--- src/App.js
+++ src/App.js
@@ -1,55 +1,25 @@
 import React, { useEffect, useState, setState } from 'react';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 import "./App.css";
-import Terminal from './components/Dungeon/Terminal.jsx'
+
+import About from "./components/About.jsx"
 
 function App(props) {
-  //const [pageContext, setPageContext] = React.createContext('game')
-  //const [roomContext, setroomContext] = React.createContext('break_unlit')
-  
-  const [pageState, setPageState] = useState("game");
-  const PageContext = React.createContext({
-    pageState: "game",
-    setPageState: () => {}
-  });
-
-  const [roomState, setRoomState] = useState("break_unlit")
-  const RoomContext = React.createContext({
-    roomState: "break_unlit",
-    setRoomState: () => {}
-  });
-
-  console.log(RoomContext)
-  const [flags, setFlags] = useState(Array(3).fill(0))
-  const FlagsContext = React.createContext({
-    flags: Array(3).fill(0),
-    setFlags: () => {}
-  });
-
-  const PageUpdater = () => {
-    const {pageState, setPageState} = React.useContext(PageContext)
-
-  }
-  const pageValue = {pageState, setPageState};
-  const roomValue = {roomState, setRoomState}
-
   return (
-    <PageContext.Provider value={PageContext.pageState}>
-      <RoomContext.Provider value={RoomContext.roomState}>
         <BrowserRouter basename="/">
           <div id="App">
-            <div className="header">
-              <span className="navButton"><Link to="about">About</Link></span>
-              <span className="navButton"><Link to="projects">Projects</Link></span>
-              <span className="navButton"><Link to="/">Game</Link></span>
-              <span className="navButton"><Link to="stuff">Stuff</Link></span>
-              <span className="navButton"><Link to="writings">Writings</Link></span>
+            <div class="header">
+              <span id="about" data-hover="Experientia"><Link to="experience" className="navButton">Experience</Link></span>
+              <Link to="projects"><span id="projects" class="navButton" data-hover="Prodita">Projects</span></Link>
+              <Link to="/"><span id="about" class="navButton" data-hover="De Me">About</span></Link>
+              <span id="writings"><Link to="writings"  className="navButton">Writings</Link></span>
+              <span id="dungeon"><Link to="dungeon" className="navButton">Dungeon</Link></span>
             </div>
-            <Route component={Terminal}/>
+              <Route exact path="/">
+              <About/>
+              </Route>    
           </div>
         </BrowserRouter>
-      </RoomContext.Provider>
-    </PageContext.Provider>
   );
 }
 
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+const About = () => {
+    return (
+       <div class="container">
+          <h1>About Me</h1>
+          <p>About US page body content</p>
+       </div>
+    );
+}
+ 
+export default About;

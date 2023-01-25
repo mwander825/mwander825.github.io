b061d1dcf59616add55b3e90f979d2f8283041e6
mwander825 <mjwander211@gmail.com>
2023-01-24 23:10:13 -0500

    Adding texts and refining accordion

---
+++ src/static/Writings.js
@@ -0,0 +1,19 @@
+export default
+{
+    "Magistro": {
+      "bellowsClass": "bellows-Latin-verse",
+      "lineClass": "line-Latin-verse",
+      "content": ["Tales dies sunt,", "quos dei neglegant,", "quos humus exspectet,", "qui in nos lente ruant,", "sed non inimici sunt,", "nam omnes creati tandem delendi sunt.", "Tale die cogito,", "priores respectans,", "de te; Senex (dico summa honore),", "animo sapientissimo,", "scientias nobis veteras dans.", "Cogito, ", "nos omnes cogitamus,", "ut animis post oculos apparas,", "modo imago,", "panemque caseum mixtum vino manibus duobus tuis tenens,", "omnem diem, quo una didicimus, celebrans.", "Tibi has verbas scribo,", "sed scio te eas non posse legere.", "Non frustra scribo,", "ut litterae meae meminisse tui iuvent.", "I ita ad agros beatos,", "magister care,", "nam etsi operte (vero cineree) tamen vivis,", "et ultimis librum antiquum ferens,", "ibi te congrediar."]
+    },
+    
+    "Sine Nomine Imperfectum I": {
+        "bellowsClass": "bellows-Latin-prose",
+        "lineClass": "line-Latin-prose",
+        "content": ["Olim ego sanusque beatus iuvenis eram. Etsi aevus aliquid maneat, animus aurinus lente volat, neque infelix neque felix, ambulo orbiis, errans corpus vitae plenum nimis. Vero, hoc est nunc, non semper. Libero me cura, nescius puer (verbum quo ad infinitum nominabor) morium hominum, non cogitabam de vita futura. Nonne debemus, per teneris annis nostris, hoc modo vivere? Puto necessus rem esse, sed non recte aliquem casu vitae parare posse; namque nobis docendum est quam vivere, etiam cur. “Quam” atque “cur” dua sunt quae, candidae fulgentes, qualis astra atro radiantia caelo nocte, illis apparitis suis latibulis, ante animo cotidie pendent. Volo scire qui, si aliqui sint, vias faciant, mores cudant, itinera finivisse agnoscant. Sic alucinabor ut responsa inveniam.", "Saepe miro, sed non diu, quos suas mortalitates non umquam meditentur."]
+    },
+
+    "Feli Felici": {
+        "bellowsClass": "bellows-Latin-verse",
+        "lineClass": "line-Latin-verse",
+        "content": ["Quid persaepe (semper, fortasse)", "\u201cHomini amicus maximus canis est\u201d", "loquantur?", "Triste vero \u201cHomini carissima qua bestia tenera est\u201d", "loquor. An nos, qui homunculi vastare ", "silvas et montes et orbis omnes terras", "terrarum solemus, talibus bestiis", "dignati sumus? ", "An nesciunt illae, parvae gremio natae suave", "nostrae Terrae carae,", "hominem esse quem omnia,", "omnia quae viridiaque virentia sunt,", "audacia magna ruat?", "Quidquid sit causa,", "tu fideliter apud me mansisti;", "neque servus, neque capta,", "at socius errans per iter ullum.", "Etsi ultro tu errabas, ibas cum me", "Et ibam cum te.", "Delicium (hoc nomine te vocem?)", "Amicus est.", "Delicium (\u201cbestiam\u201d dicam?)", "Ipsum vivens est.", "Delicium, vero,", "Bestia tenera est,", "Et dum tibi spiritum,", "Quod validus erat olim, fessum exspirans.", "Etsi nescio agro quo tandem erras", "etsi ustum, in ligno stantem, te tuor", "et fixus miror plenam mortis urnam."]   }
+}
